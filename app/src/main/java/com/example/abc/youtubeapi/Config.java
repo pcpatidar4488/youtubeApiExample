7fc5d4fe5480a2964366b10fdcd350678b992347
PUNAMCHAND <punam.chand@berylsystem.com>
2018-03-26 19:39:14 +0530

    Youtube api

---
+++ app/src/main/java/com/example/abc/youtubeapi/Config.java
@@ -0,0 +1,15 @@
+package com.example.abc.youtubeapi;
+
+/**
+ * Created by abc on 3/26/2018.
+ */
+
+public class Config {
+
+        // Google Console APIs developer key
+        // Replace this key with your's
+        public static final String DEVELOPER_KEY = "AIzaSyDjXflOA9IVCq0pPFWy6_3-u5iriQGoDfY";
+
+        // YouTube video id
+        public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
